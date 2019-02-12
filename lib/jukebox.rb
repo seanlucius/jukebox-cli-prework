4a7e3bd4f542464dc88811630f20d15a8e828679
Sean Lucius <seanlucius22@icloud.com>
2019-02-12 22:15:59 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -19,5 +19,5 @@ def help
 end 
 
 def list(songs)
-  songs.each_with_index { |song, index| puts "#{index + 1}. #{song}"}
+  songs.each_with_index { |song, index| puts "#{index + 1}. #{song}" }
 end 
