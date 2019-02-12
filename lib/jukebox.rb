dd7d1267d7eeea7043d4da1c3d6d88125b7620c9
Sean Lucius <seanlucius22@icloud.com>
2019-02-12 22:09:33 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,5 +12,8 @@ songs = [
 
 def help 
   puts "I accept the following commands:
-  "
+  - help : displays this help message
+  - list : displays a list of songs you can displays
+  - play : lets you choose a song to play
+  - exit : exits this program"
 end 
